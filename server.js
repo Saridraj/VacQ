5dbfd43b5489e4a4cd9d11c0e393428523c979e9
saridraj <saridrajsapsin@gmail.com>
2022-02-06 23:21:02 +0700

    Connect with mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+
 
 //Route files
 const hospitals = require ('./routes/hospitals');
@@ -7,9 +10,21 @@ const hospitals = require ('./routes/hospitals');
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
+//Connect to database
+connectDB();
+
 const app=express();
+//Body parser
+app.use(express.json());
 
 app.use('/api/v1/hospitals',hospitals);
 
 const PORT=process.env.PORT || 3000;
-app.listen(PORT, console.log('Server runnig in', process.env.NODE_ENV, 'mode on port', PORT));
+const server = app.listen(PORT, console.log('Server runnig in', process.env.NODE_ENV, 'mode on port', PORT));
+
+//Handle unhandle promise rejection
+process.on('unhandleRejection' ,(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(()=>process.exit(1));
+});
