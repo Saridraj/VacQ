ffbb3b24208dcffd76a8201feff4da76f1f00a29
saridraj <saridrajsapsin@gmail.com>
2022-02-16 00:27:46 +0700

    Authentication

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser=require('cookie-parser');
 const connectDB = require('./config/db');
 
-
-
-//Route files
-const hospitals = require ('./routes/hospitals');
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
@@ -13,11 +10,22 @@ dotenv.config({path:'./config/config.env'});
 //Connect to database
 connectDB();
 
+//Route files
+const hospitals = require ('./routes/hospitals');
+const auth = require('./routes/auth');
+
 const app=express();
+
 //Body parser
 app.use(express.json());
 
+//Cookie parser
+app.use(cookieParser());
+
 app.use('/api/v1/hospitals',hospitals);
+app.use('/api/v1/auth',auth);
+
+
 
 const PORT=process.env.PORT || 3000;
 const server = app.listen(PORT, console.log('Server runnig in', process.env.NODE_ENV, 'mode on port', PORT));
